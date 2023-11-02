787910a502f585fc2eaac3118d7a35e1fd8e0dfe
Anas Ahmad <ahmedanas8032@gmail.com>
2023-11-02 23:57:41 +0530

    rollback

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -44,89 +44,99 @@ const MyChats = ({ fetchAgain }) => {
     };
 
 
+    // // Function to filter chats based on search input
+    // const filteredChats = chats.filter((chat) =>
+    //     chat.isGroupChat
+    //         ? chat.chatName.toLowerCase().includes(searchUser.toLowerCase())
+    //         : getSender(loggedUser, chat.users).toLowerCase().includes(searchUser.toLowerCase())
+    // );
 
     return (
-        <Box
-            display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
-            flexDir="column"
-            // alignItems="center"
-            p={3}
-            bg="white"
-            w={{ base: "100%", md: "31%" }}
-            borderRadius="lg"
-            borderWidth="1px"
-        >
+        <>
             <Box
-                pb={3}
-                px={3}
-                fontSize={{ base: "28px", md: "30px" }}
-                fontFamily="Work sans"
-                display="flex"
-                w="100%"
-                justifyContent="space-between"
-                alignItems="center"
-            >
-                <Text fontSize={{ base: "22px", md: "30px" }}>My Chats</Text>
-                <GroupChatModal>
-                    <Button
-                        display="flex"
-                        fontSize={{ base: "17px", md: "30px", lg: "17px" }}
-                        rightIcon={<AddIcon />}
-                    >
-                        New Group Chat
-                    </Button>
-                </GroupChatModal>
-            </Box>
-            <Box
-                display="flex"
+                display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
                 flexDir="column"
+                // alignItems="center"
                 p={3}
-                bg="#F8F8F8"
-                w="100%"
-                h="100%"
+                bg="white"
+                w={{ base: "100%", md: "31%" }}
                 borderRadius="lg"
-                overflowY="hidden"
+                borderWidth="1px"
             >
-                {selectedChat && chats ? (
-                    <Stack overflowY="scroll">
-                        {chats.map((chat) => (
-                            <Box
-                                onClick={() => setSelectedChat(chat)}
-                                cursor="pointer"
-                                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                                color={selectedChat === chat ? "white" : "black"}
-                                px={3}
-                                py={2}
-                                borderRadius="lg"
-                                key={chat._id}
-                            >
-                                <Text>
-                                    {!chat.isGroupChat
-                                        ? getSender(loggedUser, chat.users)
-                                        : chat.chatName}
-                                </Text>
-                                {chat.latestMessage && (
-                                    <Text fontSize="xs">
-                                        <b>{chat.latestMessage.sender.name} : </b>
-                                        {chat.latestMessage.content.length > 50
-                                            ? chat.latestMessage.content.substring(0, 51) + "..."
-                                            : chat.latestMessage.content}
-                                    </Text>
-                                )}
-                            </Box>
-                        ))}
-                    </Stack>
-                ) : (
-                    // <><ChatLoading /></>
-                    <Box display="flex" alignItems="center" justifyContent="center" h="100%">
-                        <Text fontSize="3xl" pb={3} fontFamily="Work sans">
-                            Search for user to start chatting...
-                        </Text>
-                    </Box>
+                <Box
+                    pb={3}
+                    px={3}
+                    fontSize={{ base: "28px", md: "30px" }}
+                    fontFamily="Work sans"
+                    display="flex"
+                    w="100%"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Text fontSize={{ base: "22px", md: "30px" }}>My Chats</Text>
+                    <GroupChatModal>
+                        <Button
+                            display="flex"
+                            fontSize={{ base: "17px", md: "30px", lg: "17px" }}
+                            rightIcon={<AddIcon />}
+                        >
+                            New Group Chat
+                        </Button>
+                    </GroupChatModal>
+                </Box>
+                <Box
+                    display="flex"
+                    flexDir="column"
+                    p={3}
+                    bg="#F8F8F8"
+                    w="100%"
+                    h="100%"
+                    borderRadius="lg"
+                    overflowY="hidden"
+                >
+                    {chats ? (
+                        <>
+                            <Stack overflowY="scroll">
+                                {chats.map((chat) => (
+                                    <Box
+                                        onClick={() => setSelectedChat(chat)}
+                                        cursor="pointer"
+                                        bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                                        color={selectedChat === chat ? "white" : "black"}
+                                        px={3}
+                                        py={2}
+                                        borderRadius="lg"
+                                        key={chat._id}
+                                    >
+                                        <Text>
+                                            {!chat.isGroupChat
+                                                ? getSender(loggedUser, chat.users)
+                                                : chat.chatName}
+                                        </Text>
+                                        {chat.latestMessage && (
+                                            <Text fontSize="xs">
+                                                <b>{chat.latestMessage.sender.name} : </b>
+                                                {chat.latestMessage.content.length > 50
+                                                    ? chat.latestMessage.content.substring(0, 51) + "..."
+                                                    : chat.latestMessage.content}
+                                            </Text>
+                                        )}
+                                    </Box>
+                                ))}
+                            </Stack>
+                        </>
+                    ) : (
+                        // <><ChatLoading /></>
+                        <Box display="flex" alignItems="center" justifyContent="center" h="100%">
+                            <Text fontSize="3xl" pb={3} fontFamily="Work sans">
+                                Search for user to start chatting...
+                            </Text>
+                        </Box>
 
-                )}
+                    )}
+                </Box>
             </Box>
-        </Box>
+        </>
     );
 };
 
