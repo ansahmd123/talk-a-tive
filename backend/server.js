7647da3fd6fd58cd0c0e4b56a83658e652d6745d
Anas Ahmad <ahmedanas8032@gmail.com>
2024-09-21 19:28:55 +0530

    Trying deployment setup

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 process.env.NODE_ENV = "production";
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/frontend/jenkins-build")));
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "frontend", "jenkins-build", "index.html"))
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
     );
 } else {
     app.get("/", (req, res) => {
